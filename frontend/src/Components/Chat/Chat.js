b2999d6b45b5ca614b9defa25b95631edf410f90
Brian Zhang <bria.zhang@mail.utoronto.ca>
2022-10-29 02:47:38 -0400

    Implemented modal for login

--- frontend/src/Components/Chat/Chat.js
+++ frontend/src/Components/Chat/Chat.js
@@ -1,4 +1,5 @@
 import {Box, VStack} from "@chakra-ui/react";
+import ChatOpenSurvey from "./ChatOpenSurvey";
 import ChatBoxTopNav from "./ChatBoxTopNav";
 import ChatBox from "./ChatBox";
 import ChatBoxFooter from "./ChatBoxFooter";
@@ -8,6 +9,7 @@ import ChatBoxFooter from "./ChatBoxFooter";
 const Chat = (props) => {
   return (
     <Box bgColor={'white'} overflow={'hidden'} mt={5} borderTopRadius={'lg'} borderBottomRadius={'lg'} ml={'10vw'} mr={'10vw'} mb={'30vh'}>
+      <ChatOpenSurvey/>
       <ChatBoxTopNav courseCode={props.courseCode}/>
       <ChatBox/>
       <ChatBoxFooter/>
