4ee846f991a45ab47a2d7e89e2b3938bd0047af1
Marko Choi <markochoi1010@gmail.com>
2022-11-19 00:12:11 -0500

    implemented researcher most-common-words endpoint with YAKE

--- backend/quickTA/students/functions/common_topics.py
+++ backend/quickTA/students/functions/common_topics.py
@@ -6,43 +6,12 @@ def generate_wordcloud(data):
     
     # Join all sentences together
     text = " ".join(sentence for sentence in data)
-    
-    kw_extractor = yake.KeywordExtractor()
+
+    # Parameter specification
+    numOfKeywords = 10
+
+    kw_extractor = yake.KeywordExtractor(top=numOfKeywords)
     keywords = kw_extractor.extract_keywords(text)
-
-    for kw in keywords:
-        print(kw)
-
-
-    # List of words to ignore
-    # stopwords = set(STOPWORDS)
-    # General terms
-    # stopwords.update([
-    #     'says', 'know', 'will', 'stop', 'without', 
-    #     'appear', 'us', 'think', 'help', 'may', 'want',
-    #     'wondering', 'long', 'higher', 'noticed', 'small', 'sense',
-    #     'probably', 'use', 'lot', "o'", 'task', 'run',
-    #     'set', 'understand', 'unable', 'represents', 'affect',
-    #     'works', 'actually', 'trouble', 'question', 'find', 'doesnt',
-    #     'sure', 'others', 'title', 'even', 'going', 'causes',
-    #     'okay', 'different', 'around', 'Edit', 'almost', 'fall',
-    #     'fine', 'center', 'code', 'likely', 'helps', 'give', 'change',
-    #     'likely', 'decide', 'part', 'understanding', 'pick', 'whole',
-    #     'make', 'codes', 'many', 'avoid', 'possible', 'lab',
-    #     'appreciated', 'appears', 'reading', 'confused', 'result',
-    #     'return', 'Now', 'temporarily', 'times', 'changing',
-    #     'Using'
-    # ])
-
-    # niche terms
-    # stopwords.update([
-    #     'function', 'data'
-    # ])
-    # Create word cloud object
-    # wordcloud = WordCloud(stopwords=stopwords, background_color="white")
     
-    # Generate and visualize the word cloud
-    # wc_img = wordcloud.generate(text)
-    # wc_img.to_file("img/wordcloud/wordcloud_{}.png".format(datetime.now()))
-    # wc_img.to_svg("img/wordcloud/wordcloud_{}.svg".format(datetime.now()))
-    
+    return keywords
+
--- backend/quickTA/students/views/researcher_views.py
+++ backend/quickTA/students/views/researcher_views.py
@@ -14,7 +14,7 @@ from ..serializers.serializers import ConversationSerializer
 from ..serializers.researcher_serializers import ResearchersSerializer, ReportedListSerializer, AverageRatingSerializer, ChatlogListSerializer, ResponseRateSerializer, MostCommonWordsSerializer
 from ..serializers import researcher_serializers as rs
 from drf_yasg.utils import swagger_auto_schema
-# from ..functions.common_topics import generate_wordcloud
+from ..functions.common_topics import generate_wordcloud
 # Create your views here.
 class ResearchersView(generics.CreateAPIView):
     queryset = Chatlog.objects.all()
@@ -420,13 +420,14 @@ def get_most_common_words(request):
                 if chatlog.is_user:
                     sentences.append(chatlog.chatlog)
         
-#         generate_wordcloud(sentences)
+        words = generate_wordcloud(sentences)
 
         response = {
             "avg_chatlog_count": sum(chatlog_count) / convo_count,
             "total_chatlog_count": sum(chatlog_count),
             "avg_chatlog_length": sum([len(chatlog) for chatlog in sentences]) / sum(chatlog_count),
-            "sentences": sentences
+            "sentences": sentences,
+            "most_common_words": words
         }
         return Response(response, status=status.HTTP_200_OK)
 
