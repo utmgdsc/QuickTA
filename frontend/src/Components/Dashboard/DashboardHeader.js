c4aabb85e6f023f41976b82f7f4e29fc1595b33b
Brian Zhang <bria.zhang@mail.utoronto.ca>
2022-12-01 02:09:03 -0500

    add course creator elements

--- frontend/src/Components/Dashboard/DashboardHeader.js
+++ frontend/src/Components/Dashboard/DashboardHeader.js
@@ -1,10 +1,20 @@
-import { Box, Heading, Text } from "@chakra-ui/react"
+import { 
+    Box, 
+    Heading, 
+    Text, 
+    HStack,
+    Button 
+} from "@chakra-ui/react"
 import CourseSelect from "../CourseSelect";
+import CourseCreator from "./CourseCreator";
 
 const DashboardHeader = ({courseCode, courseName}) => {
     return (
         <Box mt={4}>
-            <CourseSelect/>
+            <HStack>
+                <CourseSelect/>
+                <CourseCreator/>
+            </HStack>
             <Heading as='h1' size="lg" color='#2C54A7' lineHeight='0.9' mt={5}>{courseCode}: {courseName}</Heading>
             <Text size='lg'>Admin Dashboard</Text>
         </Box>
