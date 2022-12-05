7cc22e9fe412f4fc25ea03067a3d4ba30cccdfb1
Brian Zhang <bria.zhang@mail.utoronto.ca>
2022-12-05 01:04:03 -0500

    implement model creator components

--- frontend/src/Pages/ResearcherModels.js
+++ frontend/src/Pages/ResearcherModels.js
@@ -1,8 +1,30 @@
+import { 
+    Box, 
+    Heading, 
+    Text, 
+    HStack,
+    Button, 
+    ModalHeader
+} from "@chakra-ui/react"
+import TopNav from "../Components/TopNav";
+import ModelHeader from "../Components/ModelManager/ModelHeader"
+import ModelBody from "../Components/ModelManager/ModelBody"
 
 
-
-const ResearcherModels = () => {
-    
-}
+const ResearcherModels = ({UTORid, courseCode, courseName, courseid}) => {
+    return (
+        <div style={{
+            backgroundColor: "#F1F1F1",
+            width: "100vw",
+            height: '100vh'
+          }}>
+            <TopNav UTORid={UTORid}/>
+            <Box ml={'12vw'} mr={'12vw'}>
+                <ModelHeader courseCode={"CSC108"} courseName={"Introduction to Computer Programming"}/>
+                <ModelBody />
+            </Box>
+        </div>
+    );
+};
 
 export default ResearcherModels;
