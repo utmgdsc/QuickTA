fa2410d675122f0c50e8e03d467fd118b7f698e9
Marko Choi <markochoi1010@gmail.com>
2022-12-15 17:27:15 -0500

    Unifed formatting of swagger docs for all admin view endpoint

--- backend/quickTA/students/serializers/admin_serializers.py
+++ backend/quickTA/students/serializers/admin_serializers.py
@@ -1,12 +1,48 @@
 from rest_framework import serializers
 from ..models import User
 
-class CreateUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['user_id', 'name', 'utorid', 'user_role']
+# Create User Request
+# ===========================================================
+class CreateOneUserRequest(serializers.Serializer):
+    name = serializers.CharField()
+    utorid = serializers.CharField()
+    user_role = serializers.CharField()
+
+class CreateOneUserResponse(serializers.Serializer):
+    user_id = serializers.CharField()
+    name = serializers.CharField()
+    utorid = serializers.CharField()
+    user_role = serializers.CharField()
+
+# Create Multiple Users Request
+# ===========================================================
+class CreateMultipleUserRequest(serializers.Serializer):
+    users = CreateOneUserRequest(many=True)
+
+class CreateMultipleUserResponse(serializers.Serializer):
+    added = CreateOneUserResponse(many=True)
+
+# Add User to Course Request
+# ===========================================================
+class AddUserToCourseRequest(serializers.Serializer):
+    user_id = serializers.CharField()
+    course_id = serializers.CharField()
+    type = serializers.CharField()
+
+# Add Multiple Users to Course Request
+# ===========================================================
+class AddMultipleUserToCourseRequest(serializers.Serializer):
     
-class AddUserCourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['user_id']
+    class UsersToCourse(serializers.Serializer):
+        user_id = serializers.CharField()
+
+    users = UsersToCourse(many=True)
+    course_id = serializers.CharField()
+    type = serializers.CharField()
+
+# Remove User from Course Request
+# ===========================================================
+class RemoveUserFromCourseRequest(serializers.Serializer):
+    user_id = serializers.CharField()
+    course_id = serializers.CharField()
+    type = serializers.CharField()
