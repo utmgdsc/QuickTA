2e1fb842e8ceffa63399d467c1c5a9dbca1d0b55
Marko <markochoi1010@gmail.com>
2023-09-26 23:48:06 +0800

    fix

--- backend/quickTA/quickTA/settings.py
+++ backend/quickTA/quickTA/settings.py
@@ -114,7 +114,7 @@ DATABASES = {
         'ENFORCE_SCHEMA': False,
         'CLIENT': {
             # 'host': 'mongodb+srv://admin:cEWQJjkyDAracCLY@quickta.dmbx3ix.mongodb.net/?retryWrites=true&w=majority'
-            'host': 'mongodb+srv://admin:Xtew2UVZQbexVsYw@quickta.jypfcah.mongodb.net/?retryWrites=true&w=majority'
+            'host': env('MONGODB')
             # 'host': 'mongodb://127.0.0.1:27017'
         },
         'OPTIONS': {
