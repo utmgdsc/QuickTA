b6fee55d1fa7c17db1ba792c4b89414212f33587
Hammad Sheikh <hammad.shaeque@gmail.com>
2023-09-30 14:51:15 -0400

    Scrollbar styling

--- frontend/src/assets/theme.js
+++ frontend/src/assets/theme.js
@@ -5,6 +5,24 @@ const theme = extendTheme({
     heading: "Poppins",
     body: "Poppins",
   },
+  styles: {
+    global: {
+      "::-webkit-scrollbar": {
+        width: "4px", /* Width of the scrollbar */
+      },
+      "::-webkit-scrollbar-track": {
+        background: "#f1f1f1", /* Background color of the track */
+      },
+      "::-webkit-scrollbar-thumb": {
+        background: "#012E8A", /* Background color of the thumb */
+        borderRadius: "6px", /* Rounded corners for the thumb */
+      },
+      "::-webkit-scrollbar-thumb:hover": {
+        background: "#555", /* Background color on hover */
+      },
+      
+    },
+  },
 })
 
 export default theme
